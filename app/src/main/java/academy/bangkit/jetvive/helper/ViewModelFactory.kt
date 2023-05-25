ecdf28032e69dc4a246013965db08fe27419d273
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-26 00:08:03 +0700

    refactor: update view model factory

--- app/src/main/java/academy/bangkit/jetvive/helper/ViewModelFactory.kt
+++ app/src/main/java/academy/bangkit/jetvive/helper/ViewModelFactory.kt
@@ -1,15 +1,21 @@
 package academy.bangkit.jetvive.helper
 
-import academy.bangkit.jetvive.data.repository.TouristAttractionRepository
-import academy.bangkit.jetvive.data.repository.UserRepository
+import academy.bangkit.jetvive.data.repository.OnboardingRepository
+import academy.bangkit.jetvive.ui.screen.onboarding.OnboardingViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class ViewModelFactory(
-    private val userRepository: UserRepository,
-    private val touristAttractionRepository: TouristAttractionRepository
+    private val onboardingRepository: OnboardingRepository
     ): ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return super.create(modelClass)
+        return when {
+            modelClass.isAssignableFrom(OnboardingViewModel::class.java) -> {
+                OnboardingViewModel(onboardingRepository) as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+        }
     }
 }
