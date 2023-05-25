4c62ba51c70d80373cd894a1608651146e65aa41
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-25 20:33:36 +0700

    build: onboarding repository

---
+++ app/src/main/java/academy/bangkit/jetvive/data/repository/OnboardingRepository.kt
@@ -0,0 +1,24 @@
+package academy.bangkit.jetvive.data.repository
+
+import academy.bangkit.jetvive.model.FakeOnboardingDataSource
+import academy.bangkit.jetvive.model.Onboarding
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class OnboardingRepository {
+
+    fun getAllOnBoardingData(): Flow<List<Onboarding>> =
+        flowOf(FakeOnboardingDataSource.dummyOnboardings)
+
+    companion object {
+        @Volatile
+        private var instance: OnboardingRepository? = null
+
+        fun getInstance(): OnboardingRepository =
+            instance ?: synchronized(this) {
+                OnboardingRepository().apply {
+                    instance = this
+                }
+            }
+    }
+}
