55e941000619c36be96b60f4f2b4c8f2087ee3d5
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-25 17:22:00 +0700

    style: setup color theme

--- app/src/main/java/academy/bangkit/jetvive/ui/theme/Color.kt
+++ app/src/main/java/academy/bangkit/jetvive/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightNavy = Color(0xffa5d7e8)
+val SemiLightNavy = Color(0xff576cbc)
+val SemiDarkNavy = Color(0xff19376d)
+val DarkNavy = Color(0xff0b2447)
