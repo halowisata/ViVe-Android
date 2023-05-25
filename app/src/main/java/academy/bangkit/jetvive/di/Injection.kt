a965f072fd38c29fc625df6e5b4e604ea6ebe538
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-25 20:32:41 +0700

    refactor: add onboarding repository injection

--- app/src/main/java/academy/bangkit/jetvive/di/Injection.kt
+++ app/src/main/java/academy/bangkit/jetvive/di/Injection.kt
@@ -1,10 +1,15 @@
 package academy.bangkit.jetvive.di
 
+import academy.bangkit.jetvive.data.repository.OnboardingRepository
 import academy.bangkit.jetvive.data.repository.TouristAttractionRepository
 import academy.bangkit.jetvive.data.repository.UserRepository
 
 
 object Injection {
+    fun provideOnBoardingRepository(): OnboardingRepository {
+        return OnboardingRepository.getInstance()
+    }
+
     fun provideUserRepository(): UserRepository {
         return UserRepository.getInstance()
     }
