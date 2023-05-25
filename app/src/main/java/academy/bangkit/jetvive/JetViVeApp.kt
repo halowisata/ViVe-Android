9d89eab0b8b1dbcda2440a41409361567116cb88
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-25 19:16:06 +0700

    build: splash screen view with dummy logo

--- app/src/main/java/academy/bangkit/jetvive/JetViVeApp.kt
+++ app/src/main/java/academy/bangkit/jetvive/JetViVeApp.kt
@@ -1,2 +1,25 @@
 package academy.bangkit.jetvive
 
+import academy.bangkit.jetvive.ui.screen.splash.SplashScreen
+import academy.bangkit.jetvive.ui.theme.JetViVeTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun JetViVeApp(
+    modifier: Modifier = Modifier
+) {
+    Surface {
+        SplashScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JetViVeAppPreview() {
+    JetViVeTheme {
+        JetViVeApp()
+    }
+}
