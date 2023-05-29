7674ea10815469ecd111139c9dec82632cfe0b1f
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-29 20:06:32 +0700

    style: fix login screen

--- app/src/main/java/academy/bangkit/jetvive/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/academy/bangkit/jetvive/ui/screen/login/LoginScreen.kt
@@ -1,8 +1,11 @@
 package academy.bangkit.jetvive.ui.screen.login
 
 import academy.bangkit.jetvive.R
+import academy.bangkit.jetvive.helper.ViewModelFactory
 import academy.bangkit.jetvive.ui.theme.JetViVeTheme
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +21,7 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -27,28 +31,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 
 @Composable
-fun LoginScreen() {
-    LoginContent()
+fun LoginScreen(
+    navigateToSignUp: () -> Unit
+) {
+    LoginContent(navigateToSignUp)
 }
 
 @Composable
 fun LoginContent(
+    navigateToSignUp: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -58,26 +72,53 @@ fun LoginContent(
     ) {
         TopSection()
         LoginForm()
+        BottomSection(navigateToSignUp)
     }
+}
+
+@Composable
+fun TopSection(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(R.drawable.sign_in),
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = 30.dp,
+                end = 30.dp,
+                top = 90.dp,
+                bottom = 30.dp
+            )
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginForm(
+    viewModel: LoginViewModel = viewModel(
+        factory = ViewModelFactory.getInstance(context = LocalContext.current)
+    ),
     modifier: Modifier = Modifier
 ) {
     Column(
         modifier = Modifier
-            .padding(30.dp),
-        verticalArrangement = Arrangement.spacedBy(15.dp)
+            .padding(30.dp)
+            .fillMaxHeight(.9f),
+        verticalArrangement = Arrangement.spacedBy(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = stringResource(R.string.sign_in),
+            fontSize = 26.sp,
+            lineHeight = 35.sp,
             modifier = Modifier
                 .fillMaxWidth()
         )
         var email by remember { mutableStateOf(TextFieldValue("")) }
         OutlinedTextField(
+            shape = RoundedCornerShape(10.dp),
             value = email,
             leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = null) },
             //trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) },
@@ -85,16 +126,18 @@ fun LoginForm(
                 email = it
             },
             label = { Text(text = "Email") },
-            placeholder = { Text(text = "johndoe@example.com") },
             singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
             modifier = Modifier
-                .clip(RoundedCornerShape(5.dp))
                 .fillMaxWidth()
         )
         var password by remember { mutableStateOf(TextFieldValue("")) }
-        var passwordVisible by rememberSaveable { mutableStateOf(false) }
+        var showPassword by remember { mutableStateOf(value = false) }
         OutlinedTextField(
+            shape = RoundedCornerShape(10.dp),
             value = password,
             leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = null) },
             //trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) },
@@ -102,75 +145,137 @@ fun LoginForm(
                 password = it
             },
             label = { Text(text = "Password") },
-            placeholder = { Text(text = "********") },
             singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
             trailingIcon = {
-                val image = if (passwordVisible)
-                    Icons.Filled.Visibility
-                else Icons.Filled.VisibilityOff
-
-                // Please provide localized description for accessibility services
-                val description = if (passwordVisible) "Hide password" else "Show password"
-
-                IconButton(onClick = {passwordVisible = !passwordVisible}){
-                    Icon(imageVector  = image, description)
+                if (showPassword) {
+                    IconButton(onClick = { showPassword = false }) {
+                        Icon(
+                            imageVector = Icons.Filled.Visibility,
+                            contentDescription = "hide_password"
+                        )
+                    }
+                } else {
+                    IconButton(
+                        onClick = { showPassword = true }) {
+                        Icon(
+                            imageVector = Icons.Filled.VisibilityOff,
+                            contentDescription = "hide_password"
+                        )
+                    }
                 }
             },
             modifier = Modifier
-                .clip(RoundedCornerShape(5.dp))
                 .fillMaxWidth()
         )
         Text(
             text = stringResource(R.string.forget_password),
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            lineHeight = 18.sp,
+            letterSpacing = .25.sp,
+            style = TextStyle(color = Color(0xFF576CBC)),
             textAlign = TextAlign.End,
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(bottom = 10.dp)
         )
         Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
+            verticalArrangement = Arrangement.spacedBy(5.dp)
         ) {
+            val coroutineScope = rememberCoroutineScope()
             Button(
-                onClick = {},
-
-                ) {
+                onClick = {
+                  coroutineScope.launch {
+                      viewModel.login(email.text, password.text)
+                  }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
                 Text(
-                    text = "Login",
+                    text = stringResource(R.string.sign_in),
                     style = TextStyle(
                         color = Color.White
                     )
                 )
             }
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(7.dp)
+            Text(
+                text = stringResource(R.string.or),
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Button(
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFFFFF)),
+                border = BorderStroke(1.dp, Color(0xFF79747E)),
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
             ) {
-                Text(text = "New to ViVe?")
-                Text(text = "Sign Up")
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(5.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.google),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(24.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.sign_in_with_google),
+                        color = Color(0xFF79747E)
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun TopSection() {
-    Image(
-        painter = painterResource(R.drawable.jetpack_compose),
-        contentDescription = null,
+fun BottomSection(
+    navigateToSignUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxWidth()
-            .size(300.dp)
-    )
+    ) {
+        Text(
+            text = stringResource(R.string.new_to_vive),
+            fontSize = 14.sp,
+            lineHeight = 18.sp,
+            letterSpacing = .25.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(end = 3.dp)
+        )
+        Text(
+            text = stringResource(R.string.sign_up),
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center,
+            lineHeight = 18.sp,
+            letterSpacing = .25.sp,
+            style = TextStyle(color = Color(0xFF576CBC)),
+            modifier = Modifier
+                .clickable {
+                    navigateToSignUp()
+                }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun LoginContentPreview() {
     JetViVeTheme {
-        LoginContent()
+        LoginContent(navigateToSignUp = {})
     }
 }
