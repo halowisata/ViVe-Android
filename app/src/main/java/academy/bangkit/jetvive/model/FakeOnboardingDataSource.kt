875ab838b63ac91352b6200c796fe558f595bc4f
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-29 09:46:30 +0700

    refactor: update resource image data

--- app/src/main/java/academy/bangkit/jetvive/model/FakeOnboardingDataSource.kt
+++ app/src/main/java/academy/bangkit/jetvive/model/FakeOnboardingDataSource.kt
@@ -1,21 +1,23 @@
 package academy.bangkit.jetvive.model
 
+import academy.bangkit.jetvive.R
+
 object FakeOnboardingDataSource {
     val dummyOnboardings = listOf(
         Onboarding(
-            1,
+            R.drawable.onboarding_1,
             "Discover Your Perfect Gateway",
             "Find your dream travel destinations effortlessly with our mood-based " +
                     "recommender system"
         ),
         Onboarding(
-            2,
+            R.drawable.onboarding_2,
             "Set Your Mood, Find Your Adventure",
             "Tell us how you feel and let us curate personalized travel recommendations " +
                     "just for you"
         ),
         Onboarding(
-            3,
+            R.drawable.onboarding_3,
             "Explore, Experience, Enjoy",
             "Embark on unforgottable journeys tailored to your mood and uncover hidden " +
                     "gems around the world"
