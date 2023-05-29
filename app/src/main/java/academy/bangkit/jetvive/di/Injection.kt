74d670be8acee49bb0a666e349dae46d0f2c3c74
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-29 20:05:05 +0700

    refactor: update injection for user repository

--- app/src/main/java/academy/bangkit/jetvive/di/Injection.kt
+++ app/src/main/java/academy/bangkit/jetvive/di/Injection.kt
@@ -1,17 +1,33 @@
 package academy.bangkit.jetvive.di
 
+import academy.bangkit.jetvive.data.local.datastore.UserPreferences
 import academy.bangkit.jetvive.data.repository.OnboardingRepository
 import academy.bangkit.jetvive.data.repository.TouristAttractionRepository
 import academy.bangkit.jetvive.data.repository.UserRepository
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 
 
 object Injection {
+
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+        name = "user_preferences"
+    )
+
     fun provideOnboardingRepository(): OnboardingRepository {
         return OnboardingRepository.getInstance()
     }
 
-    fun provideUserRepository(): UserRepository {
-        return UserRepository.getInstance()
+    private fun provideUserPreferences(context: Context): UserPreferences {
+        val dataStore = context.dataStore
+
+        return UserPreferences.getInstance(dataStore)
+    }
+
+    fun provideUserRepository(context: Context): UserRepository {
+        return UserRepository.getInstance(provideUserPreferences(context))
     }
 
     fun provideTouristAttractionRepository(): TouristAttractionRepository {
