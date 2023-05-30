3c837f8e5734c6a1ed588552a3ab9ae64eefee26
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-30 12:20:55 +0700

    build: initialize tourist attraction item component

---
+++ app/src/main/java/academy/bangkit/jetvive/ui/components/TouristAttractionItem.kt
@@ -0,0 +1,18 @@
+package academy.bangkit.jetvive.ui.components
+
+import academy.bangkit.jetvive.ui.theme.JetViVeTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TouristAttractionItem() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TouristAttractionItemPreview() {
+    JetViVeTheme {
+        TouristAttractionItem()
+    }
+}
