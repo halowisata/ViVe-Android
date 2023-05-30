665e14b70148b7b4a1809e3d0e6ab28df0347ed9
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-30 21:38:53 +0700

    refactor: create get all tourist attractions method

--- app/src/main/java/academy/bangkit/jetvive/data/repository/TouristAttractionRepository.kt
+++ app/src/main/java/academy/bangkit/jetvive/data/repository/TouristAttractionRepository.kt
@@ -1,6 +1,14 @@
 package academy.bangkit.jetvive.data.repository
 
+import academy.bangkit.jetvive.model.tourist_attraction.FakeTouristAttractionDataSource
+import academy.bangkit.jetvive.model.tourist_attraction.TouristAttraction
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
 class TouristAttractionRepository {
+
+    fun getAllTouristAttractions(): Flow<List<TouristAttraction>> =
+        flowOf(FakeTouristAttractionDataSource.dummyTouristAttractions)
 
     companion object {
         @Volatile
