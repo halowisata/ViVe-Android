b4dddea4864c2a407ff6314e6556d4f2c860d235
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-30 15:36:33 +0700

    build: fake tourist attraction data source

---
+++ app/src/main/java/academy/bangkit/jetvive/model/tourist_attraction/FakeTouristAttractionDataSource.kt
@@ -0,0 +1,7 @@
+package academy.bangkit.jetvive.model.tourist_attraction
+
+class FakeTouristAttractionDataSource {
+    val dummyTouristAttractions = listOf(
+        TouristAttraction("tourist_attraction-1")
+    )
+}
