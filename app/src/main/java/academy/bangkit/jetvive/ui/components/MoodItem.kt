6e0b194b741636c5608479005326fc98445400fa
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-30 14:09:53 +0700

    style: update text weight

--- app/src/main/java/academy/bangkit/jetvive/ui/components/MoodItem.kt
+++ app/src/main/java/academy/bangkit/jetvive/ui/components/MoodItem.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -53,6 +54,7 @@ fun MoodItem(
             )
             Text(
                 text = text,
+                fontWeight = FontWeight.SemiBold,
                 fontSize = 20.sp,
                 lineHeight = 30.sp,
                 letterSpacing = .15.sp
