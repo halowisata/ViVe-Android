b852324a95446b3c585b7c085a3c43802afd27d3
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-30 12:18:32 +0700

    build: entity

---
+++ app/src/main/java/academy/bangkit/jetvive/data/local/entity/UserEntity.kt
@@ -0,0 +1,7 @@
+package academy.bangkit.jetvive.data.local.entity
+
+data class UserEntity(
+    val id: String,
+    val name: String,
+    val token: String
+)
---
+++ app/src/main/java/academy/bangkit/jetvive/model/user/User.kt
@@ -0,0 +1,8 @@
+package academy.bangkit.jetvive.model.user
+
+data class User(
+    val id: String,
+    val name: String,
+    val email: String,
+    val password: String
+)
