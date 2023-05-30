721385c7a006e664b1b962b78ae3897434d81f58
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-30 12:19:31 +0700

    refactor: create login method with fake data

--- app/src/main/java/academy/bangkit/jetvive/data/repository/UserRepository.kt
+++ app/src/main/java/academy/bangkit/jetvive/data/repository/UserRepository.kt
@@ -1,14 +1,43 @@
 package academy.bangkit.jetvive.data.repository
 
-class UserRepository {
+import academy.bangkit.jetvive.data.local.datastore.UserPreferences
+import academy.bangkit.jetvive.data.local.entity.UserEntity
+import academy.bangkit.jetvive.model.user.FakeUserDataSource
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.flow.Flow
+
+class UserRepository(private val userPreferences: UserPreferences) {
+
+    suspend fun login(email: String, password: String): Boolean {
+        var loginState = false
+
+        FakeUserDataSource.dummyUsers.forEach { dummyUser ->
+            if (email == dummyUser.email && password == dummyUser.password) {
+                userPreferences.setLogin(UserEntity(
+                    dummyUser.id,
+                    dummyUser.name,
+                    "dummyToken")
+                )
+                loginState = true
+            }
+        }
+
+        return if (loginState) {
+            loginState
+        } else {
+            loginState
+        }
+    }
+
+    fun getLogin(): Flow<UserEntity> = userPreferences.getLogin()
 
     companion object {
         @Volatile
         private var instance: UserRepository? = null
 
-        fun getInstance(): UserRepository =
+        fun getInstance(userPreferences: UserPreferences): UserRepository =
             instance ?: synchronized(this) {
-                UserRepository().apply {
+                UserRepository(userPreferences).apply {
                     instance = this
                 }
             }
