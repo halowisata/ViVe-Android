dfe709f8b937240735f39b0a3f92b4df2300a27c
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-30 14:11:20 +0700

    refactor: add mood repository injection to view model factory

--- app/src/main/java/academy/bangkit/jetvive/helper/ViewModelFactory.kt
+++ app/src/main/java/academy/bangkit/jetvive/helper/ViewModelFactory.kt
@@ -1,8 +1,10 @@
 package academy.bangkit.jetvive.helper
 
+import academy.bangkit.jetvive.data.repository.MoodRepository
 import academy.bangkit.jetvive.data.repository.OnboardingRepository
 import academy.bangkit.jetvive.data.repository.UserRepository
 import academy.bangkit.jetvive.di.Injection
+import academy.bangkit.jetvive.ui.screen.home.HomeViewModel
 import academy.bangkit.jetvive.ui.screen.login.LoginViewModel
 import academy.bangkit.jetvive.ui.screen.onboarding.OnboardingViewModel
 import android.content.Context
@@ -11,17 +13,24 @@ import androidx.lifecycle.ViewModelProvider
 
 class ViewModelFactory(
     private val onboardingRepository: OnboardingRepository,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val moodRepository: MoodRepository
     ): ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
+            modelClass.isAssignableFrom(academy.bangkit.jetvive.ViewModel::class.java) -> {
+                academy.bangkit.jetvive.ViewModel(userRepository) as T
+            }
             modelClass.isAssignableFrom(OnboardingViewModel::class.java) -> {
                 OnboardingViewModel(onboardingRepository) as T
             }
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
                 LoginViewModel(userRepository) as T
+            }
+            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
+                HomeViewModel(moodRepository) as T
             }
             else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
         }
@@ -35,7 +44,8 @@ class ViewModelFactory(
             instance ?: synchronized(this) {
                 instance ?: ViewModelFactory(
                     Injection.provideOnboardingRepository(),
-                    Injection.provideUserRepository(context)
+                    Injection.provideUserRepository(context),
+                    Injection.provideMoodRepository()
                 )
             }.also { instance = it }
     }
