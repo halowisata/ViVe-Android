df1f07d1a1fe28bfe22117e9f9c21302fa04c739
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-31 19:06:45 +0700

    refactor: build profile screen content

--- app/src/main/java/academy/bangkit/jetvive/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/academy/bangkit/jetvive/ui/screen/profile/ProfileScreen.kt
@@ -1,8 +1,31 @@
 package academy.bangkit.jetvive.ui.screen.profile
 
+import academy.bangkit.jetvive.R
 import academy.bangkit.jetvive.ui.theme.JetViVeTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProfileScreen() {
@@ -10,14 +33,85 @@ fun ProfileScreen() {
 }
 
 @Composable
-fun ProfileContent() {
-
+fun ProfileContent(
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = stringResource(R.string.back),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { onBackClick() }
+                )
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.bookmark),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { onBackClick() }
+                )
+            }
+            Image(
+                painter = painterResource(R.drawable.jetpack_compose),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(150.dp)
+            )
+            Text(
+                text = "John Doe"
+            )
+            Text(
+                text = "Bali, Indonesia"
+            )
+        }
+        Column(
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null
+                )
+                Text(
+                    text = "+62 895-3819-00306"
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Email, contentDescription = null)
+                Text(
+                    text = "johndoe@example.com"
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ProfileContentPreview() {
     JetViVeTheme {
-        ProfileContent()
+        ProfileContent(
+            onBackClick = {},
+        )
     }
 }
