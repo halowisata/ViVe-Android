485c404c9c9b4ab1f94a0e06e29b98ca1ca091d4
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-02 09:35:02 +0700

    refactor: update model property

--- app/src/main/java/academy/bangkit/jetvive/model/user/User.kt
+++ app/src/main/java/academy/bangkit/jetvive/model/user/User.kt
@@ -3,7 +3,10 @@ package academy.bangkit.jetvive.model.user
 data class User(
     val id: String,
     val name: String,
+    val username: String,
     val email: String,
     val password: String,
-    val moodId: String? = null
+    val phoneNumber: String,
+    val address: String,
+    val gender: String
 )
