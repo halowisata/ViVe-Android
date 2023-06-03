953ed04b1d8eab1dab346f39d81dd1bd47683287
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-04 03:56:38 +0700

    build: alert feature

---
+++ app/src/main/java/academy/bangkit/jetvive/ui/components/Alert.kt
@@ -0,0 +1,59 @@
+package academy.bangkit.jetvive.ui.components
+
+import academy.bangkit.jetvive.R
+import academy.bangkit.jetvive.ui.theme.JetViVeTheme
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Alert(
+    title: String,
+    name: String,
+    showDialog: Boolean,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    if (showDialog) {
+        AlertDialog(
+            title = {
+                Text(title)
+            },
+            text = {
+                Text(text = name)
+            },
+            onDismissRequest = onDismiss,
+            confirmButton = {
+                TextButton(
+                    onClick = onConfirm
+                ) {
+                    Text(stringResource(R.string.yes))
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = onDismiss
+                ) {
+                    Text(stringResource(R.string.cancel))
+                }
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlertPreview() {
+    JetViVeTheme {
+        Alert(
+            title = stringResource(R.string.alert_title),
+            name = stringResource(R.string.alert_name),
+            showDialog = true,
+            onConfirm = {},
+            onDismiss = {}
+        )
+    }
+}
