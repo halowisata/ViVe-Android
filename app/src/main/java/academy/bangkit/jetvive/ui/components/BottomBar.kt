1434858936481bad6a93eb9fcabda16e950e29dd
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-04 01:29:06 +0700

    build: bottom bar

---
+++ app/src/main/java/academy/bangkit/jetvive/ui/components/BottomBar.kt
@@ -0,0 +1,66 @@
+package academy.bangkit.jetvive.ui.components
+
+import academy.bangkit.jetvive.R
+import academy.bangkit.jetvive.ui.navigation.NavigationItem
+import academy.bangkit.jetvive.ui.navigation.Screen
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomBar(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    BottomAppBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        val navigationItems = listOf(
+            NavigationItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home,
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_bookmark),
+                icon = Icons.Default.Bookmark,
+                screen = Screen.Bookmark
+            )
+        )
+
+        BottomAppBar {
+            navigationItems.map { item ->
+                NavigationBarItem(
+                    selected = currentRoute == item.screen.route,
+                    onClick = {
+                        navController.navigate(item.screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            restoreState = true
+                            launchSingleTop = true
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = stringResource(R.string.item_icon)
+                        )
+                    },
+                    label = { Text(item.title) },
+                )
+            }
+        }
+    }
+}
