753cc8423a98490a3572a1a06be2cdbfe7c26617
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-05 00:38:29 +0700

    refactor: add back handler

--- app/src/main/java/academy/bangkit/jetvive/helper/Utils.kt
+++ app/src/main/java/academy/bangkit/jetvive/helper/Utils.kt
@@ -1,6 +1,16 @@
 package academy.bangkit.jetvive.helper
 
 import academy.bangkit.jetvive.R
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.delay
 
 fun getPeriod(hour: Int) = when (hour) {
     in 6..11 -> {
@@ -17,5 +27,36 @@ fun getPeriod(hour: Int) = when (hour) {
     }
     else -> {
         R.string.morning
+    }
+}
+
+sealed class BackPress {
+    object Idle : BackPress()
+    object InitialTouch : BackPress()
+}
+
+@Composable
+fun TwiceBackPressExit() {
+    var showToast by remember { mutableStateOf(false) }
+
+    var backPressState by remember { mutableStateOf<BackPress>(BackPress.Idle) }
+    val context = LocalContext.current
+
+    if(showToast){
+        Toast.makeText(context, R.string.press_back_again_to_exit, Toast.LENGTH_SHORT).show()
+        showToast= false
+    }
+
+
+    LaunchedEffect(key1 = backPressState) {
+        if (backPressState == BackPress.InitialTouch) {
+            delay(2000)
+            backPressState = BackPress.Idle
+        }
+    }
+
+    BackHandler(backPressState == BackPress.Idle) {
+        backPressState = BackPress.InitialTouch
+        showToast = true
     }
 }
