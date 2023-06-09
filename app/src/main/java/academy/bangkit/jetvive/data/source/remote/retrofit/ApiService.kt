5fa77d8e39a48eb6dc418840caae88021f7d8d7d
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-09 22:41:15 +0700

    build: setup retrofit network

---
+++ app/src/main/java/academy/bangkit/jetvive/data/source/remote/retrofit/ApiService.kt
@@ -0,0 +1,12 @@
+package academy.bangkit.jetvive.data.source.remote.retrofit
+
+import academy.bangkit.jetvive.data.source.remote.request.RegisterRequest
+import academy.bangkit.jetvive.data.source.remote.response.RegisterResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @POST("/users")
+    suspend fun register(@Body registerRequest: RegisterRequest): RegisterResponse
+}
