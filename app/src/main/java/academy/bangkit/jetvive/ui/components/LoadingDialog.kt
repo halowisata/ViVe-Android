ef2a9fa3c9c55746f5c731de1c0163d1ad3b04e3
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-11 00:36:08 +0700

    feat: create loading dialog

---
+++ app/src/main/java/academy/bangkit/jetvive/ui/components/LoadingDialog.kt
@@ -0,0 +1,45 @@
+package academy.bangkit.jetvive.ui.components
+
+import academy.bangkit.jetvive.ui.theme.JetViVeTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+@Composable
+fun LoadingDialog() {
+    Dialog(
+        onDismissRequest = {},
+        properties = DialogProperties(
+            dismissOnClickOutside = false,
+            dismissOnBackPress = false
+        )
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .background(Color.Transparent)
+                .fillMaxSize()
+        ) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingDialogPreview() {
+    JetViVeTheme {
+        LoadingDialog()
+    }
+}
