6b00ced57bb72fe820b1eb814a6c3b6086cae00b
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-10 23:15:56 +0700

    refactor: add register method

--- app/src/main/java/academy/bangkit/jetvive/ui/screen/register/RegisterViewModel.kt
+++ app/src/main/java/academy/bangkit/jetvive/ui/screen/register/RegisterViewModel.kt
@@ -1,6 +1,28 @@
 package academy.bangkit.jetvive.ui.screen.register
 
 import academy.bangkit.jetvive.data.repository.UserRepository
+import academy.bangkit.jetvive.data.source.remote.request.RegisterRequest
+import academy.bangkit.jetvive.data.source.remote.response.RegisterResponse
+import academy.bangkit.jetvive.ui.common.UiState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class RegisterViewModel(private val userRepository: UserRepository): ViewModel() {}
+class RegisterViewModel(private val userRepository: UserRepository): ViewModel() {
+    private val _registrationStatus = MutableStateFlow<UiState<RegisterResponse>>(UiState.Loading)
+    val registrationStatus: StateFlow<UiState<RegisterResponse>> get() = _registrationStatus
+
+    fun register(registerRequest: RegisterRequest) {
+        viewModelScope.launch {
+            _registrationStatus.value = UiState.Loading
+            val uiState = userRepository.register(registerRequest)
+            _registrationStatus.value = when (uiState) {
+                is UiState.Success -> UiState.Success(uiState.data)
+                is UiState.Error -> UiState.Error(uiState.errorMessage)
+                UiState.Loading -> UiState.Loading
+            }
+        }
+    }
+}
