33a2e86216831be9e04cf26bd6c0f894fb463d8c
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-10 23:13:02 +0700

    refactor: wrap the return api with response method

--- app/src/main/java/academy/bangkit/jetvive/data/source/remote/retrofit/ApiService.kt
+++ app/src/main/java/academy/bangkit/jetvive/data/source/remote/retrofit/ApiService.kt
@@ -2,11 +2,12 @@ package academy.bangkit.jetvive.data.source.remote.retrofit
 
 import academy.bangkit.jetvive.data.source.remote.request.RegisterRequest
 import academy.bangkit.jetvive.data.source.remote.response.RegisterResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
 
-    @POST("/users")
-    suspend fun register(@Body registerRequest: RegisterRequest): RegisterResponse
+    @POST("users")
+    suspend fun register(@Body registerRequest: RegisterRequest): Response<RegisterResponse>
 }
