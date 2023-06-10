16495ae02b70d3bd7565580d9ed830f8611056a7
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-10 23:12:07 +0700

    refactor: remove unused file

--- app/src/main/java/academy/bangkit/jetvive/data/source/remote/retrofit/ApiResponse.kt
+++
@@ -1,7 +0,0 @@
-package academy.bangkit.jetvive.data.source.remote.retrofit
-
-sealed class ApiResponse<out R> private constructor() {
-    data class Success<out T>(val data: T): ApiResponse<T>()
-    data class Error(val error: String): ApiResponse<Nothing>()
-    object Empty: ApiResponse<Nothing>()
-}
