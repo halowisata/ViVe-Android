ecc8ee7c44c0c27a0fe0c4706ad139bcac24cbe5
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-11 10:28:42 +0700

    feat: create get login data

--- app/src/main/java/academy/bangkit/jetvive/main/MainViewModel.kt
+++ app/src/main/java/academy/bangkit/jetvive/main/MainViewModel.kt
@@ -1,10 +1,12 @@
 package academy.bangkit.jetvive.main
 
 import academy.bangkit.jetvive.data.repository.UserRepository
+import academy.bangkit.jetvive.data.source.local.entity.UserEntity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
@@ -13,10 +15,23 @@ class MainViewModel(private val userRepository: UserRepository): ViewModel() {
     private val mutableStateFlow = MutableStateFlow(true)
     val isLoading = mutableStateFlow.asStateFlow()
 
+    private val _loginData = MutableStateFlow<UserEntity?>(null)
+    val loginData: StateFlow<UserEntity?> get() = _loginData
+
     init {
         viewModelScope.launch {
             delay(2000)
             mutableStateFlow.value = false
+        }
+
+        getLogin()
+    }
+
+    fun getLogin() {
+        viewModelScope.launch {
+            userRepository.getLogin().collect { userEntity ->
+                _loginData.value = userEntity
+            }
         }
     }
 }
