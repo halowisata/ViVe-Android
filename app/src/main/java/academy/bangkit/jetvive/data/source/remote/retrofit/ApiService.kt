7072bacd3b301d22a31f9abcefb1864ffef062b5
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-11 12:46:34 +0700

    refactor: create logout method by call api

--- app/src/main/java/academy/bangkit/jetvive/data/source/remote/retrofit/ApiService.kt
+++ app/src/main/java/academy/bangkit/jetvive/data/source/remote/retrofit/ApiService.kt
@@ -1,11 +1,14 @@
 package academy.bangkit.jetvive.data.source.remote.retrofit
 
 import academy.bangkit.jetvive.data.source.remote.request.LoginRequest
+import academy.bangkit.jetvive.data.source.remote.request.LogoutRequest
 import academy.bangkit.jetvive.data.source.remote.request.RegisterRequest
 import academy.bangkit.jetvive.data.source.remote.response.LoginResponse
+import academy.bangkit.jetvive.data.source.remote.response.LogoutResponse
 import academy.bangkit.jetvive.data.source.remote.response.RegisterResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.HTTP
 import retrofit2.http.POST
 
 interface ApiService {
@@ -15,4 +18,7 @@ interface ApiService {
 
     @POST("authentications")
     suspend fun login(@Body loginRequest: LoginRequest): Response<LoginResponse>
+
+    @HTTP(method = "DELETE", path = "authentications", hasBody = true)
+    suspend fun logout(@Body logoutRequest: LogoutRequest): Response<LogoutResponse>
 }
