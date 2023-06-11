cbd8e0a3ede6bbf78aacaa7f03a154e64070c47c
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-11 12:57:42 +0700

    refactor: create login and logout method

--- app/src/main/java/academy/bangkit/jetvive/data/repository/UserRepository.kt
+++ app/src/main/java/academy/bangkit/jetvive/data/repository/UserRepository.kt
@@ -2,7 +2,11 @@ package academy.bangkit.jetvive.data.repository
 
 import academy.bangkit.jetvive.data.source.local.datastore.UserPreferences
 import academy.bangkit.jetvive.data.source.local.entity.UserEntity
+import academy.bangkit.jetvive.data.source.remote.request.LoginRequest
+import academy.bangkit.jetvive.data.source.remote.request.LogoutRequest
 import academy.bangkit.jetvive.data.source.remote.request.RegisterRequest
+import academy.bangkit.jetvive.data.source.remote.response.LoginResponse
+import academy.bangkit.jetvive.data.source.remote.response.LogoutResponse
 import academy.bangkit.jetvive.data.source.remote.response.RegisterResponse
 import academy.bangkit.jetvive.data.source.remote.retrofit.ApiService
 import academy.bangkit.jetvive.ui.common.UiState
@@ -26,8 +30,36 @@ class UserRepository(
         }
     }
 
+    suspend fun login(loginRequest: LoginRequest): UiState<LoginResponse> {
+        return try {
+            val response = apiService.login(loginRequest)
+            if (response.isSuccessful) {
+                UiState.Success(response.body() ?: throw Exception("Empty response body"))
+            } else {
+                UiState.Error("Login failed")
+            }
+        } catch (exception: Exception) {
+            UiState.Error(exception.message.toString())
+        }
+    }
+
     suspend fun setLogin(userEntity: UserEntity) = userPreferences.setLogin(userEntity)
+
     fun getLogin(): Flow<UserEntity> = userPreferences.getLogin()
+
+    suspend fun logout(logoutRequest: LogoutRequest): UiState<LogoutResponse> {
+        return try {
+            val response = apiService.logout(logoutRequest)
+            if (response.isSuccessful) {
+                UiState.Success(response.body() ?: throw Exception("Empty response body"))
+            } else {
+                UiState.Error("Logout failed")
+            }
+        } catch (exception: Exception) {
+            UiState.Error(exception.message.toString())
+        }
+    }
+
     suspend fun deleteLogin() = userPreferences.deleteLogin()
 
     companion object {
