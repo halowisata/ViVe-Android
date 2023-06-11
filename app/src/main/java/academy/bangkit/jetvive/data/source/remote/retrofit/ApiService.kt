ab3944e56f618f8f92bc756f6f16c4e9c5df9a26
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-11 09:25:01 +0700

    refactor: add post authentication api method

--- app/src/main/java/academy/bangkit/jetvive/data/source/remote/retrofit/ApiService.kt
+++ app/src/main/java/academy/bangkit/jetvive/data/source/remote/retrofit/ApiService.kt
@@ -1,6 +1,8 @@
 package academy.bangkit.jetvive.data.source.remote.retrofit
 
+import academy.bangkit.jetvive.data.source.remote.request.LoginRequest
 import academy.bangkit.jetvive.data.source.remote.request.RegisterRequest
+import academy.bangkit.jetvive.data.source.remote.response.LoginResponse
 import academy.bangkit.jetvive.data.source.remote.response.RegisterResponse
 import retrofit2.Response
 import retrofit2.http.Body
@@ -10,4 +12,7 @@ interface ApiService {
 
     @POST("users")
     suspend fun register(@Body registerRequest: RegisterRequest): Response<RegisterResponse>
+
+    @POST("authentications")
+    suspend fun login(@Body loginRequest: LoginRequest): Response<LoginResponse>
 }
