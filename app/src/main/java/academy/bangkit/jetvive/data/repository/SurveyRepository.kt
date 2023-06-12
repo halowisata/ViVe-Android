248b0bf4f525f7c02e683ba1751910559ecacb9a
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-12 22:06:42 +0700

    refactor: update add survey method consume api

--- app/src/main/java/academy/bangkit/jetvive/data/repository/SurveyRepository.kt
+++ app/src/main/java/academy/bangkit/jetvive/data/repository/SurveyRepository.kt
@@ -1,31 +1,34 @@
 package academy.bangkit.jetvive.data.repository
 
 import academy.bangkit.jetvive.data.source.remote.request.SurveyRequest
-import academy.bangkit.jetvive.model.survey.Survey
+import academy.bangkit.jetvive.data.source.remote.response.SurveyResponse
+import academy.bangkit.jetvive.data.source.remote.retrofit.ApiService
+import academy.bangkit.jetvive.ui.common.UiState
 
-class SurveyRepository {
+class SurveyRepository(private val apiService: ApiService) {
 
-    private val surveys = mutableListOf<Survey>()
-
-    fun addSurvey(surveyRequest: SurveyRequest) {
-        surveys.add(
-            Survey(
-                id = "survey",
-                userId = surveyRequest.userId,
-                moodId = surveyRequest.moodId,
-                budget = surveyRequest.budget,
-                travelDistance = surveyRequest.travelDistance,
-                destinationCity = surveyRequest.destinationCity
+    suspend fun addSurvey(accessToken: String, surveyRequest: SurveyRequest): UiState<SurveyResponse> {
+        return try {
+            val response = apiService.addSurvey(
+                "Bearer $accessToken",
+                surveyRequest
             )
-        )
+            if (response.isSuccessful) {
+                UiState.Success(response.body() ?: throw Exception("Empty response body"))
+            } else {
+                UiState.Error("Add survey failed")
+            }
+        } catch (exception: Exception) {
+            UiState.Error(exception.message.toString())
+        }
     }
 
     companion object {
         @Volatile
         private var instance: SurveyRepository? = null
 
-        fun getInstance(): SurveyRepository = instance ?: synchronized(this) {
-            SurveyRepository().apply {
+        fun getInstance(apiService: ApiService): SurveyRepository = instance ?: synchronized(this) {
+            SurveyRepository(apiService).apply {
                 instance = this
             }
         }
